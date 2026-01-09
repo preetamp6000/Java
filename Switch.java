2ed7dcaf3ba1ba167017f9aaf4b99ae3e0785293
preetamp6000 <ppreetam627@gmail.com>
2026-01-09 20:20:12 +0530

    first commit

---
+++ Switch.java
@@ -0,0 +1,81 @@
+// import java.util.Scanner;
+// public class Switch 
+// {
+//     public static void main(String args[])
+//         {
+//             int day;
+//             System.out.println("Enter a Day");
+//             Scanner obj=new Scanner(System.in);
+//             day=obj.nextInt();
+//             switch(day){
+//                 case 1:
+//                     System.out.println("Monday");
+//                 break;
+
+//                 case 2:
+//                     System.out.println("Tuesday");
+//                 break;
+
+//                 case 3:
+//                     System.out.println("Wednesday");
+//                 break;
+
+//                 case 4:
+//                     System.out.println("Thrusday");
+//                 break;
+
+//                 case 5:
+//                     System.out.println("Friday");
+//                 break;
+
+//                 case 6:
+//                     System.out.println("Saturday");
+//                 break;
+
+//                 case 7:
+//                     System.out.println("Sunday");
+//                 break;
+
+//                 default:
+//                     System.out.println("Not a Day");
+//             }
+
+//         }
+    
+// }
+
+
+// Calculator
+
+
+import java.util.Scanner;
+class Switch{
+    public static void main(String args[])
+        {
+            int a,b;
+            String ch;
+            System.out.println("Enter a Number");
+            Scanner obj=new Scanner(System.in);
+            a=obj.nextInt();
+            b=obj.nextInt();
+            System.out.println("Enter Operator");
+            obj.nextLine();
+            ch=obj.nextLine();
+            switch(ch){
+                case "+": System.out.println(a+b);
+                break;
+                case "-": System.out.println(a-b);
+                break;
+                case "/": System.out.println(a/b);
+                break;
+                case "*": System.out.println(a*b);
+                break;
+                default:
+                    System.out.println("Invaild");
+
+            }
+
+
+        }
+    
+}
---
+++ sample.java
@@ -0,0 +1,92 @@
+// import java.util.Scanner;
+// public class sample{
+//     public static void main(String args[])
+//     {
+//         int n;
+//         System.out.println("Enter a number");
+//         Scanner obj=new Scanner(System.in);
+//         n=obj.nextInt();
+//         if(n > 0){
+//             System.out.println("+ve number");
+//         }
+//         else if(n == 0){
+//             System.out.println("neither +ve nor -ve");
+//         }
+//         else{
+//             System.out.println("-ve number");
+//         }
+
+//     } 
+    
+// }
+
+
+
+// Greater Number Program
+
+// import java.util.Scanner;
+// class sample{
+    
+//         public static void main(String args[])
+//         {
+//             int a,b;
+//             System.out.println("Enter two numbers");
+//             Scanner obj=new Scanner(System.in);
+//             a=obj.nextInt();
+//             b=obj.nextInt();
+//             if(a > b){
+//                 System.out.println(a);
+//             }
+//             else{
+//                 System.out.println(b);
+
+//             }
+
+        
+//     }
+// }
+
+
+
+// Ladder Program
+
+// import java.util.Scanner;
+// class sample{
+//     public static void main(String args[])
+//     {
+//       int a,b,c; 
+//       System.out.println("Enter three numbers");
+//       Scanner obj=new Scanner(System.in);
+//       a=obj.nextInt();
+//       b=obj.nextInt();
+//       c=obj.nextInt();
+//       if(a > b){
+//         if(a>c)
+//             {
+//                 System.out.println(a);
+
+//         }
+//         else{
+//             System.out.println(c);
+
+
+//         }
+
+//       }
+//       else{
+//         if(b > c){
+//             System.out.println(b);
+//         }
+//         else{
+//             System.out.println(c);
+
+//         }
+//       }
+
+
+//     }
+// }
+
+
+
+

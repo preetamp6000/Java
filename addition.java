9c47a4086bff6a637944e5655e16a0c9c25b5448
preetamp6000 <ppreetam627@gmail.com>
2026-01-09 18:48:02 +0530

    first commit

---
+++ addition.java
@@ -0,0 +1,42 @@
+// public class addition
+//  {
+    
+//     public static void main(String []args)
+//         {
+//             int a=10 ,b=20;
+//             int sum= a+b;
+//             System.out.println("Addition"+ sum);
+
+//         }   
+// }
+
+// import java.util.Scanner;
+// public class addition
+//  {
+    
+//     public static void main(String []args)
+//         {
+//             int a,b;
+//             System.out.println("Enter two number");
+//             Scanner obj=new Scanner(System.in);
+//             a=obj.nextInt();
+//             b=obj.nextInt();
+//             System.out.println(a+b);
+
+//         }   
+//}
+
+import java.util.Scanner;
+class addition{
+    public static void main(String args[])
+        {
+            String name;
+            System.out.println("Enter your name");
+            Scanner obj=new Scanner(System.in);
+            name=obj.nextLine();
+            System.out.println("Name:" +name);
+
+
+        }
+    
+}

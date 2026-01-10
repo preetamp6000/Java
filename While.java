3b4e44745274a42eb39819e4a6c53a911dae4e25
preetamp6000 <ppreetam627@gmail.com>
2026-01-10 22:53:43 +0530

    first commit

---
+++ While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 10) {
+            System.out.println("2 * " + n + " = " + (2 * n));
+            n++;
+        }
+    }
+}
+           

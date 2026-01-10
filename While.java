44cb94903e9b7eb2c6515ffd4ecd2b1e9e2912d9
preetamp6000 <ppreetam627@gmail.com>
2026-01-10 23:10:42 +0530

    first commit

--- While.java
+++ While.java
@@ -1,3 +1,5 @@
+//While loops
+
 public class While {
     public static void main(String[] args) {
         int n = 1;
@@ -7,4 +9,45 @@ public class While {
         }
     }
 }
+
+
+//Do While Loop
+
+class Dowhile {
+    public static void main(String args[]) {
+        int n = 1;
+        do {
+            System.out.println(n);
+            n++;
+        } while (n <= 10);
+    }
+}
+
+
+// For Loop
+
+class ForLoop {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+//For-each loop
+
+class ForEachExample {
+    public static void main(String args[]) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int n : numbers) {
+            System.out.println(n);
+        }
+    }
+}
+
+
+
            

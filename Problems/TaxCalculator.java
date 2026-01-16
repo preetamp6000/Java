59fd0152030a156bb50e55fdda172eca731efa52
preetamp6000 <ppreetam627@gmail.com>
2026-01-16 20:42:03 +0530

    Problem added

---
+++ Problems/TaxCalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+
+        int sal;
+        double tax;
+ 
+        Scanner r = new Scanner(System.in);
+
+        System.out.print("Enter Salary: ");
+        sal = r.nextInt();
+
+        if (sal <= 10000) {
+            tax = 0;
+        } 
+        else if (sal <= 50000) {
+            tax = sal * 0.10;
+        } 
+        else {
+            tax = sal * 0.20;
+        }
+
+        System.out.println("Salary = " + sal);
+        System.out.println("Tax = " + tax);
+
+        r.close();
+    }
+}

8a3663fe499bc20759cf66e4b062deb4fe821d3f
preetamp6000 <pritampatil6000@gmail.com>
2026-01-18 23:43:48 +0530

    Update TaxCalculator.java

--- Problems/TaxCalculator.java
+++ Problems/TaxCalculator.java
@@ -27,3 +27,42 @@ public class TaxCalculator {
         r.close();
     }
 }
+
+
+// while loop
+
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+
+        int sal;
+        double tax;
+        Scanner r = new Scanner(System.in);
+
+        // do-while loop for valid salary input
+        do {
+            System.out.print("Enter Salary (positive value): ");
+            sal = r.nextInt();
+
+            if (sal < 0) {
+                System.out.println("Salary cannot be negative. Try again.");
+            }
+        } while (sal < 0);
+
+        if (sal <= 10000) {
+            tax = 0;
+        } 
+        else if (sal <= 50000) {
+            tax = sal * 0.10;
+        } 
+        else {
+            tax = sal * 0.20;
+        }
+
+        System.out.println("Salary = " + sal);
+        System.out.println("Tax = " + tax);
+
+        r.close();
+    }
+}

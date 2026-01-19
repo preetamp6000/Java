1d5b07d4c523e99a6c2bb81295c6a45cd12e6605
preetamp6000 <pritampatil6000@gmail.com>
2026-01-19 23:48:58 +0530

    Update Armstrong.java

--- Problems/Armstrong.java
+++ Problems/Armstrong.java
@@ -26,3 +26,35 @@ public class Armstrong {
     }
     
 }
+
+
+
+// Nore than 4 digits
+
+
+import java.util.Scanner;
+
+public class ArmstrongGeneral {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n, digits = 0;
+
+        // count digits
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = n;
+        int sum = 0;
+
+        while (temp > 0) {
+            int r = temp % 10;
+            sum += Math.pow(r, digits);
+            temp /= 10;
+        }
+
+        System.out.println(sum == n ? "Armstrong" : "Not Armstrong");
+    }
+}

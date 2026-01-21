1e9cae13c4b5671ce5585ea595fa29159091adfe
preetamp6000 <ppreetam627@gmail.com>
2026-01-21 23:25:11 +0530

    Problem array

---
+++ Arrays/Length .java
@@ -0,0 +1,10 @@
+public class Length  {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 15, 25, 35};
+
+        System.out.println("Length of array: " + arr.length);
+    }
+    
+}

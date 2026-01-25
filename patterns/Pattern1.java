03bc6771a6c97e9aa7771ce66c7a927ead8ee5dc
preetamp6000 <pritampatil6000@gmail.com>
2026-01-25 23:27:13 +0530

    Update Pattern1.java

--- patterns/Pattern1.java
+++ patterns/Pattern1.java
@@ -25,3 +25,23 @@ public class Pattern1 {
         }
     }
 }
+
+
+
+
+//2
+
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a No.: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("* ".repeat(i));
+        }
+    }
+}

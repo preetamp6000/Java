ae49d9d6307061d39da262efbcd3f404a5524c49
preetamp6000 <pritampatil6000@gmail.com>
2026-01-27 23:50:29 +0530

    Update While.java

--- While.java
+++ While.java
@@ -1,57 +1,57 @@
-<<<<<<< HEAD
+
 // //While loops
 =======
 //Loops in Java
 //While loops
->>>>>>> 6de91d768662fcaff3fda60261932fb1a0c086c3
 
-// public class While {
-//     public static void main(String[] args) {
-//         int n = 1;
-//         while (n <= 10) {
-//             System.out.println("2 * " + n + " = " + (2 * n));
-//             n++;
-//         }
-//     }
-// }
+
+ public class While {
+     public static void main(String[] args) {
+         int n = 1;
+         while (n <= 10) {
+             System.out.println("2 * " + n + " = " + (2 * n));
+             n++;
+         }
+     }
+ }
 
 
 // //Do While Loop
 
-// class Dowhile {
-//     public static void main(String args[]) {
-//         int n = 1;
-//         do {
-//             System.out.println(n);
-//             n++;
-//         } while (n <= 10);
-//     }
-// }
+ class Dowhile {
+     public static void main(String args[]) {
+         int n = 1;
+         do {
+             System.out.println(n);
+             n++;
+         } while (n <= 10);
+     }
+ }
 
 
 // // For Loop
 
-// class ForLoop {
-//     public static void main(String args[]) {
-//         for (int i = 1; i <= 10; i++) {
-//             System.out.println(i);
-//         }
-//     }
-// }
+ class ForLoop {
+     public static void main(String args[]) {
+         for (int i = 1; i <= 10; i++) {
+             System.out.println(i);
+         }
+     }
+ }
 
 
 // //For-each loop
 
-// class ForEachExample {
-//     public static void main(String args[]) {
+ class ForEachExample {
+     public static void main(String args[]) {
 
-//         int[] numbers = {1, 2, 3, 4, 5};
+         int[] numbers = {1, 2, 3, 4, 5};
 
-//         for (int n : numbers) {
-//             System.out.println(n);
-//         }
-//     }
-// }
+         for (int n : numbers) {
+             System.out.println(n);
+         }
+     }
+ }
 
 
 
